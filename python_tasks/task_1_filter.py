8d310c1d8154d3d19f7433620a690b47e359cb95
Gopi Krishna <107085222+gopi76@users.noreply.github.com>
2026-08-17 15:11:12 -0400

    Update task_1_filter.py

--- python_tasks/task_1_filter.py
+++ python_tasks/task_1_filter.py
@@ -2,9 +2,21 @@
 # Instructions: Complete the function to return only even IDs 
 # greater than 100, sorted in descending order.
 
+
 def filter_orders(order_ids):
     # TODO: Write your logic here
-    pass
+
+    if not order_ids:
+        return "empty list"
+    result = []
+    for i in range(len(order_ids)):
+        if order_ids[i]>100:
+            if order_ids[i]%2==0:
+                result.append(order_ids[i])
+            else:
+                pass
+    result.sort()
+    return result[::-1]
 
 # Test Case
 test_data = [10, 105, 120, 44, 202, 300, 75, 110]
