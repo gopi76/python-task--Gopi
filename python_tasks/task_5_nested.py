54c523f4d76b7da639067f4058e2291b094a8f39
Gopi Krishna <107085222+gopi76@users.noreply.github.com>
2026-08-17 15:52:44 -0400

    Update task_5_nested.py

--- python_tasks/task_5_nested.py
+++ python_tasks/task_5_nested.py
@@ -3,7 +3,9 @@
 
 def get_vehicle_year(data):
     # TODO: Write your logic here safely
-    pass
+    
+    # we can through a loop from starting to end of data;
+    # then check for the the year in keys(). if we found out return "we have all data" else " return key is missing or unknown" Time complexity: O(n), no need of space : o(1)
 
 # Test Case
 vehicle = {'specs': {'model_info': {'year': 2024}}}
