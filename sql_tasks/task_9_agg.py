1af38ea06399ee9a4535a6e0cc4e74a245f38b22
Gopi Krishna <107085222+gopi76@users.noreply.github.com>
2026-08-17 15:46:26 -0400

    Update task_9_agg.py

--- sql_tasks/task_9_agg.py
+++ sql_tasks/task_9_agg.py
@@ -5,9 +5,7 @@ def get_popular_skus():
     cursor = conn.cursor()
     
     # Task: Return SKUs where the SUM of quantity across all orders is > 1.
-    query = """
-    -- WRITE YOUR SQL HERE
-    """
+    query = Select sku from Order_items where sum(quantity) > 1;
     
     cursor.execute(query)
     return cursor.fetchall()
